4869d3f1c196c381deb49bd57179ad96b12dabea
Badrillox <micperktold@tsn.at>
2021-08-31 18:41:06 +0200

    Discord Webhook hinzugefügt

---
+++ Maps/Maps/Maps/Services/Discord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord;
+using Discord.Webhook;
+using System.Threading.Tasks;
+namespace Maps.Services
+{
+    public class Discord
+    {
+        private string dchook = "https://discord.com/api/webhooks/882294609248665661/gFIp39DrKc7lVTb7ZUbyeczDbehaxrqtFY3MgIOiabQvLb1kbO_Qm1H-Qyjoaxde1lQT";
+        public async Task SendtoWebhook(string hwid)
+        {
+            using (var dcWeb = new DiscordWebhookClient(dchook)) 
+            {
+                var embed = new EmbedBuilder
+                {
+                    Title = "HWID - Whitelisting angefordert",
+                    Description = $"Ein HWID Whitelisting ist gerade angefordert worden mit der HWID: {hwid}"
+                };
+                await dcWeb.SendMessageAsync(text: "Discord Webhook angefordert", embeds: new[] { embed.Build() });
+            }
+        }
+    }
+}
---
+++ Maps/Maps/Maps/Services/HWID.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+namespace Maps.Services
+{
+    public class HWID
+    {
+        /*public static string GetGuid()
+        {
+            using (RegistryKey)
+        }*/
+        public static void hwid(string hwid)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                try
+                {
+                    if (wc.DownloadString("https://pastebin.com/iDhZ16FA").Contains(hwid))
+                        return;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    //Console.WriteLine("Deine HWID ist: " + GetGuid());
+                    Thread.Sleep(5000);
+                    Environment.Exit(0);
+                }
+                catch(Exception ex)
+                {
+                    Environment.Exit(0);
+                    throw ex;
+                }
+            }
+        } 
+    }
+}
