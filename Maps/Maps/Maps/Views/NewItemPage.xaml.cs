c1952796332f7f61ce0f24997202beacfc97b622
Badrillox <micperktold@tsn.at>
2021-08-31 18:11:02 +0200

    push blyat

---
+++ Maps/Maps/Maps/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Maps.Models;
+using Maps.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Maps.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
