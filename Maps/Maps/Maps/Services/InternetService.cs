b393a4dbeeb6ef8120aa60b44eaa2885cb86ddef
Badrillox <micperktold@tsn.at>
2021-09-04 19:11:59 +0200

    get connection

---
+++ Maps/Maps/Maps/Services/InternetService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+namespace Maps.Services
+{
+    // this class is meant to scan the device for an available internet connection, because we need to check the unique device id
+    class InternetService
+    {
+        public bool HasConnection()
+        {
+            return Plugin.Connectivity.CrossConnectivity.Current.IsConnected;
+        }
+    }
+}
