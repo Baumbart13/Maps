79dedf318b698e6804c48a537892f21675a0ed17
Badrillox <micperktold@tsn.at>
2021-09-04 18:52:57 +0200

    todo hinzugefügt bei HWID

--- Maps/Maps/Maps/Services/HWID.cs
+++ Maps/Maps/Maps/Services/HWID.cs
@@ -26,7 +26,8 @@ namespace Maps.Services
                         return;
                     Console.ForegroundColor = ConsoleColor.Red;
                     await dc.SendtoWebhook(DeviceUID()).ConfigureAwait(false);
-                    Console.WriteLine("Deine HWID ist: " + DeviceUID());
+                    //Console.WriteLine("Deine HWID ist: " + DeviceUID()); der Befehl muss dann anstatt hier bei 
+                    // dem Programm rein, wo es der Startbildschirm beinhaltet wird
                     Thread.Sleep(5000);
                     Environment.Exit(0);
                 }
